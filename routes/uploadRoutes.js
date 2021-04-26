e7f0b7b5a91b9a1f907fbdd88e8896b2ee19ac63
SOUnit <workbookwork7@gmail.com>
2021-04-27 08:42:05 +0900

    added calling getSignedUrl of s3

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -9,6 +9,21 @@ const s3 = new AWS.S3({
 
 module.exports = (app) => {
   app.get('/api/upload', (req, res) => {
-    res.send({ api: 'api' });
+    // temp setting
+    req.user = { id: 'test_id', name: 'test_user' };
+
+    // image upload logic
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+    s3.getSignedUrl(
+      'putObject',
+      {
+        Bucket: 'image-upload-sample-bucket',
+        ContentType: 'jpeg',
+        Key: key,
+      },
+      (err, url) => {
+        res.send({ key, url });
+      }
+    );
   });
 };
